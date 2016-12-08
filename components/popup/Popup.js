e22cacb7e8efb9af88c0fbb23becdc52a6d6662b
Alexandr Promakh <s-promakh@ya.ru>
2016-12-08 16:47:27 +0500

    other features for popup

--- components/popup/Popup.js
+++ components/popup/Popup.js
@@ -11,24 +11,44 @@ import styles from './popup.styl'
 const Popup = (props) => {
   const { features, currentCartogram, push } = props
   let sum = 0 // считаем внутри map вместо R.reduce чтобы не бегать по массиву 2 раза
+  const { cartogramFeatures, otherFeatures } = R.groupBy((feature) => {
+    return feature.properties.layer_key === currentCartogram.layer ? 'cartogramFeatures' : 'otherFeatures'
+  }, features)
 
   return (
-    <ul className={ styles.root }>
-      {
-        features.map((feature) => {
-          const value = feature.properties[currentCartogram.property]
-          const onClick = () => push(`/${feature.properties.layer_key}/${feature.id}`)
-          sum += value
+    <div className={ styles.root }>
+      <div className={ styles.header }> Объекты картограммы </div>
+      <ul>
+        {
+          cartogramFeatures.map((feature) => {
+            const value = feature.properties[currentCartogram.property]
+            const onClick = () => push(`/${feature.properties.layer_key}/${feature.id}`)
+            sum += value
 
-          return (
-            <li key={ feature.id } className={ styles.featureListItem } onClick={ onClick }>
-              {feature.properties.name}: <b>{R.isNil(value) ? 'Нет данных' : value}</b>
-            </li>
-          )
-        })
-      }
-      { (features.length > 1) && <li>Всего: <b>{sum}</b></li> }
-    </ul>
+            return (
+              <li key={ feature.id } className={ styles.featureListItem } onClick={ onClick }>
+                {feature.properties.name}: <b>{R.isNil(value) ? 'Нет данных' : value}</b>
+              </li>
+            )
+          })
+        }
+        { (features.length > 1) && <li>Всего: <b>{sum}</b></li> }
+      </ul>
+
+      <div className={ styles.header }> Остальные объекты </div>
+      <ul>
+        {
+          otherFeatures.map((feature) => {
+            const onClick = () => push(`/${feature.properties.layer_key}/${feature.id}`)
+            return (
+              <li key={ feature.id } className={ styles.featureListItem } onClick={ onClick }>
+                {feature.properties.name}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
